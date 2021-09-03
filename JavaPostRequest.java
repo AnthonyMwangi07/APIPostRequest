a436ecaabd79a36ca336c4f7c43e9125569a00b2
AnthonyMwangi07 <64577575+AnthonyMwangi07@users.noreply.github.com>
2021-09-03 12:07:10 +0300

    Send a file Post Request to an API

---
+++ JavaPostRequest.java
@@ -0,0 +1,22 @@
+import java.io.File;
+import java.net.http.HttpResponse;
+
+import kong.unirest.Unirest;
+
+public class PostRequest {
+    public StringValue action(String POST_URL,String file)
+    {
+
+        Unirest.setTimeouts(0, 0);
+        HttpResponse<String> response = Unirest.post(POST_URL)
+        .field("file", new File(file))
+        .asString();
+
+        System.out.println(response.getBody());
+        String response1=response.getBody();
+        String response2=response1;
+
+        return new StringValue (response2);
+
+    }
+}
